29989e8bcb69f7a40c860cd3024480279d86b707
Harshpreet Singh <62651121+Keen-Coder-hs@users.noreply.github.com>
2022-07-18 12:59:48 +0530

    Update flaxblenderbotmodel.py

--- flaxblenderbotmodel.py
+++ flaxblenderbotmodel.py
@@ -20,3 +20,15 @@ inputs = tokenizer("Hello, my dog is cute", return_tensors="jax")
 outputs = model(**inputs)
 
 last_hidden_states = outputs.last_hidden_state
+
+text = "My friends are cool but they eat too many carbs."
+inputs = tokenizer(text, max_length=1024, return_tensors="jax")
+encoder_outputs = model.encode(**inputs)
+
+import jax.numpy as jnp
+
+decoder_start_token_id = model.config.decoder_start_token_id
+decoder_input_ids = jnp.ones((inputs.input_ids.shape[0], 1), dtype="i4") * decoder_start_token_id
+
+outputs = model.decode(decoder_input_ids, encoder_outputs)
+last_decoder_hidden_states = outputs.last_hidden_state
